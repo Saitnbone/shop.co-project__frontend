870aa6a2ab545fbf5d65af5379501b99fbcad1a5
Karpova Anna <anyakarpova9494@gmail.com>
2024-11-14 17:25:20 +0300

    feat: add component FullBg, Footer

---
+++ src/widgets/footer/index.tsx
@@ -0,0 +1,1 @@
+export { Footer } from './facade/facade.tsx';
---
+++ src/widgets/footer/ui/Layout.tsx
@@ -0,0 +1,80 @@
+import s from './styles.module.scss';
+import { TUiFooter } from '../types.ts';
+import { footerData } from './mockFooterData.tsx';
+import { FooterLinkGroup } from './components/footer-link/FooterLinkGroup.tsx';
+import { FullBg } from '@/shared/ui/fullBg';
+import {
+  FaCcApplePay,
+  FaCcMastercard,
+  FaCcPaypal,
+  FaCcVisa,
+  FaFacebook,
+  FaGithub,
+  FaGooglePay,
+  FaInstagram,
+  FaTwitter,
+} from 'react-icons/fa';
+
+export const UiFooter: React.FC<TUiFooter> = ({ logo }) => {
+  return (
+    <FullBg className={s.bg}>
+      <footer className={s.footer}>
+        <div className={s.footerBlock}>
+          <div className={s.blockLogo}>
+            {logo}
+            <p className={s.text}>
+              We have clothes that suits your style and which you’re proud to
+              wear. From women to men.
+            </p>
+            <div className={s.social}>
+              <a href="#" className={s.socialIcon}>
+                <FaTwitter />
+              </a>
+              <a href="#" className={s.socialIcon}>
+                <FaFacebook />
+              </a>
+              <a href="#" className={s.socialIcon}>
+                <FaInstagram />
+              </a>
+              <a href="#" className={s.socialIcon}>
+                <FaGithub />
+              </a>
+            </div>
+          </div>
+          <div className={s.blockLinks}>
+            {footerData.map((group, index) => (
+              <FooterLinkGroup
+                key={index}
+                title={group.title}
+                links={group.links}
+                className={s.link}
+              />
+            ))}
+          </div>
+          <div className={s.bottom}>
+            <p className={s.textCo}>
+              Shop.co © 2000-2023, All Rights Reserved
+            </p>
+            <div className={s.payment}>
+              <p className={s.paymentCard}>
+                <FaCcVisa />
+              </p>
+              <p className={s.paymentCard}>
+                <FaCcMastercard />
+              </p>
+              <p className={s.paymentCard}>
+                <FaCcPaypal />
+              </p>
+              <p className={s.paymentCard}>
+                <FaCcApplePay />
+              </p>
+              <p className={s.paymentCard}>
+                <FaGooglePay />
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </FullBg>
+  );
+};
