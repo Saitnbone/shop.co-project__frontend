63ddd043825b51ef19de55c049ea564729b5329a
E-Okishev <106948826+E-Okishev@users.noreply.github.com>
2024-11-18 21:57:05 +0300

    Сделал первый экран

--- src/widgets/cover/ui/Layout.tsx
+++ src/widgets/cover/ui/Layout.tsx
@@ -5,6 +5,7 @@ import {Button} from "@/shared/ui/button";
 import {ButtonColor} from "@/shared/ui/button/types.ts";
 import {Title} from "@/shared/ui/title";
 import {textColor} from "@/shared/ui/title/types.ts";
+import image from '../../../shared/assets/people.png'
 
 
 export const UiCover: React.FC = () => {
@@ -16,8 +17,11 @@ export const UiCover: React.FC = () => {
           <p className={s.text}>Browse through our diverse range of meticulously crafted garments, designed to bring out
             your individuality and cater to your sense of style.</p>
           <Button color={ButtonColor.primary} className={s.button}>Shop Now</Button>
+          <AdvantageList/>
         </div>
-        <AdvantageList/>
+        <div className={s.imageBlock}>
+          <img src={image} alt="People"/>
+        </div>
       </div>
     </FullBg>
   )
