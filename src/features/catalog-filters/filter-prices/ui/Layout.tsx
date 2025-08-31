133fd5b5d0a1a229840bea0e78be91fe21eb646c
Aleksandr Golodin <alex.gp.0632@yandex.ru>
2025-08-31 15:07:57 +0200

    added price calculator

--- src/features/catalog-filters/filter-prices/ui/Layout.tsx
+++ src/features/catalog-filters/filter-prices/ui/Layout.tsx
@@ -1,13 +1,40 @@
+import { FC, useState } from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 import s from './styles.module.scss';
 import arrowRight from '@assets/catalog-page/arrow-right.svg';
 
-export const UiFilterPrices = () => {
+export const UiFilterPrices: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [priceRange, setPriceRange] = useState<[number, number]>([0, 1000]);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className={s.filterPrices}>
-      <div className={s.filterPricesHeader}>
+      <div onClick={toggleOpen} className={s.filterPricesHeader}>
         <h2 className={s.filterPricesTitle}>Prices</h2>
         <img src={arrowRight} alt="Arrow right" />
       </div>
+      {isOpen && (
+        <div className={s.sliderContainer}>
+          <Slider
+            range
+            min={0}
+            max={1000}
+            value={priceRange}
+            onChange={(value) => setPriceRange(value as [number, number])}
+            defaultValue={[20, 50]}
+          />
+          <div className={s.rangeDisplay}>
+            <span className={s.rangeText}>
+              ${priceRange[0]} - ${priceRange[1]}
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
