00ba4716db6fbc36e7f21a5a4bf67ff3fc82f1fd
Aleksandr <golodinaleksandr@gmail.com>
2025-09-23 23:09:15 +0200

    added cart block

--- src/pages/CartPage/ui/Layout.tsx
+++ src/pages/CartPage/ui/Layout.tsx
@@ -4,6 +4,7 @@ import s from './styles.module.scss';
 export const UiCartPage = ({ OrderSummary, Cart }: CartPageProps) => {
   return (
     <main className={s.cartPage}>
+      <h2 className={s.cartPageTitle}>Your Cart</h2>
       <div className={s.cartPageContainer}>
         <OrderSummary />
         <Cart />
--- src/widgets/cart-page-widgets/cart/ui/Layout.tsx
+++ src/widgets/cart-page-widgets/cart/ui/Layout.tsx
@@ -1,3 +1,36 @@
-export const UiCart = () => { 
-  return <div>Cart</div>;
-}
+import s from './styles.module.scss';
+
+export const UiCart = () => {
+  return (
+    <div className={s.cart}>
+      <div className={s.cartContent}>
+        <h3 className={s.cartTitle}>Order Summary</h3>
+        <div className={s.costDetails}>
+          <div className={s.costDetailRow}>
+            <span className={s.costDetailLabel}>Subtotal</span>
+            <span className={s.costDetailValue}>$215.00</span>
+          </div>
+          <div className={s.costDetailRow}>
+            <span className={s.costDetailLabel}>Discount (20%)</span>
+            <span className={`${s.costDetailValue} ${s.discountValue}`}>-113$</span>
+          </div>
+          <div className={s.costDetailRow}>
+            <span className={s.costDetailLabel}>Delivery Fee</span>
+            <span className={s.costDetailValue}>$15.00</span>
+          </div>
+        </div>
+        <div>
+          <div className={s.totalAmount}>
+            <span className={s.totalLabel}>Total</span>
+            <span className={`${s.costDetailValue} ${s.totalValue}`}>$117.00</span>
+          </div>
+        </div>
+        <div className={s.promoCode}>
+          <input className={s.promoInput} type="text" placeholder='Add promo code' />
+          <button className={s.promoButton}>Apply</button>
+        </div>
+        <button className={s.checkoutButton}>Go to checkout</button>
+      </div>
+    </div>
+  );
+};
