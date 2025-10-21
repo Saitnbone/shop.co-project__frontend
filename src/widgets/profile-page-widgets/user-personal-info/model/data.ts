6fe4c96114103dadfd9a9113b8e519d0f2ce8d32
Aleksandr Golodin <alex.gp.0632@yandex.ru>
2025-10-21 18:23:04 +0200

    added user info block in profile page

---
+++ src/widgets/profile-page-widgets/user-personal-info/model/data.ts
@@ -0,0 +1,12 @@
+export const userData = {
+  id: 'user-1',
+  avatar: '',
+  name: 'John Doe',
+  email: 'john.doe@example.com',
+  address: '123 Main St, Anytown, USA',
+  phone: '+1 (555) 123-4567',
+  telegram: '@johndoe',
+  cardInfo: {
+    cardNumber: '1234 5678 9012 3456',
+  },
+};
