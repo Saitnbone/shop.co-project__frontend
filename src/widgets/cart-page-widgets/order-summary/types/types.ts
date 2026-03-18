a3957c57845847b1e26278cdae373e833a8b7509
Aleksandr Golodin <alex.gp.0632@yandex.ru>
2026-03-18 21:52:46 +0100

    feat: items in cart page integration baackend

---
+++ src/widgets/cart-page-widgets/order-summary/types/types.ts
@@ -0,0 +1,9 @@
+export interface ICartItem {
+  variantId: string;
+  quantity: number;
+  image: string;
+  name: string;
+  price: string;
+  color: string;
+  size: string;
+}
--- src/widgets/cart-page-widgets/order-summary/ui/Layout.tsx
+++ src/widgets/cart-page-widgets/order-summary/ui/Layout.tsx
@@ -1,31 +1,98 @@
-import { data } from '../model/data';
+import { useCallback, useEffect } from 'react';
+import { CartApi } from '@/features/cart/api';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState, AppDispatch } from '@/app/providers/store';
+import {
+  addItemOptimistic,
+  removeItemOptimistic,
+  fetchCartData,
+} from '@/shared/slice/cartSlice';
+import { ICartItem } from '../types/types';
 import s from './styles.module.scss';
 
 export const UiOrderSummary = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const userId = useSelector((state: RootState) => state.userInfo.userInfo?.id);
+  const cartData = useSelector((state: RootState) => state.cart);
+
+  const sendQuantityUpdate = useCallback(
+    async (newQuantity: number, cartItem: ICartItem) => {
+      if (!userId) return;
+      const delta = newQuantity - cartItem.quantity; // Используй cartItem.quantity вместо initialQuantityRef
+      if (delta === 0) return;
+
+      dispatch(addItemOptimistic({ ...cartItem, quantity: delta }));
+      try {
+        if (delta > 0) {
+          await CartApi.addQuantityCartItem(userId, cartItem.variantId, delta);
+        } else {
+          await CartApi.removeQuantityCartItem(
+            userId,
+            cartItem.variantId,
+            -delta
+          );
+        }
+        dispatch(fetchCartData(userId));
+      } catch (error) {
+        console.error('Error updating quantity:', error);
+        dispatch(removeItemOptimistic(cartItem.variantId));
+      }
+    },
+    [userId, dispatch]
+  );
+
+  const handleQuantityChange = useCallback(
+    (change: number, cartItem: ICartItem) => {
+      const newQuantity = Math.max(0, cartItem.quantity + change); // Используй cartItem.quantity
+      sendQuantityUpdate(newQuantity, cartItem);
+    },
+    [sendQuantityUpdate]
+  );
+
+  const handleRemoveItem = async (variantId: string) => {
+    if (!userId) return;
+    try {
+      await CartApi.removeCartItem(userId, variantId);
+      dispatch(removeItemOptimistic(variantId));
+      dispatch(fetchCartData(userId));
+    } catch (error) {
+      console.error('Error removing item:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (userId) {
+      dispatch(fetchCartData(userId));
+    }
+  }, [userId, dispatch]);
+
   return (
     <div className={s.orderSummary}>
       <div className={s.orderSummaryContent}>
         <ul className={s.productList}>
-          {data.map((item) => (
-            <li className={s.productItem} key={item.id}>
+          {cartData.items.map((item) => (
+            <li className={s.productItem} key={item.variantId}>
               <div className={s.productInfo}>
                 <div className={s.imageWrapper}>
                   <img
                     className={s.productImage}
                     src={item.image}
-                    alt={item.title}
+                    alt={item.name}
                   />
                 </div>
                 <div className={s.productDetails}>
                   <div className={s.productTitleRow}>
-                    <h4 className={s.productTitle}>{item.title}</h4>
-                    <div className={s.deleteIcon}>
+                    <h4 className={s.productTitle}>{item.name}</h4>
+                    <button
+                      onClick={() => handleRemoveItem(item.variantId)}
+                      className={s.deleteIcon}
+                    >
                       <img
                         className={s.deleteIconImage}
                         src="/delete-icon.svg"
                         alt="Delete icon"
                       />
-                    </div>
+                    </button>
                   </div>
                   <div className={s.productSize}>
                     <span>Size:</span>
@@ -36,11 +103,15 @@ export const UiOrderSummary = () => {
                     <span className={s.productColorValue}>{item.color}</span>
                   </div>
                   <div className={s.priceQuantityRow}>
-                    <p className={s.productPrice}>${item.price.toFixed(2)}</p>
+                    <p className={s.productPrice}>${item.price}</p>
                     <div className={s.quantityControl}>
-                      <button>-</button>
+                      <button onClick={() => handleQuantityChange(-1, item)}>
+                        -
+                      </button>
                       <span className={s.productQuantity}>{item.quantity}</span>
-                      <button>+</button>
+                      <button onClick={() => handleQuantityChange(1, item)}>
+                        +
+                      </button>
                     </div>
                   </div>
                 </div>
